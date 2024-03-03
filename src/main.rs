d940c0fa5cc5f55572d53db04869ca66710df30c
Evgenii <evgenii.sevriugin@gmail.com>
2024-03-03 12:20:00 +0100

    multi function is implemented

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 fn main() {
-    println!("Hello, evgenii! : {}", add(26,99));
+    println!("Hello, evgenii! : {} {}", add(26,99), multi(10,10));
 }
 fn add(a:i32,b:i32) -> i32 {
     a+b
+}
+fn multi(a:i32,b:i32) ->i32 {
+    a*b
 }
