ca5359fa35b8ae5c76e5aebe480c32e19f4ef9b2
Evgenii <evgenii.sevriugin@gmail.com>
2024-03-05 19:35:22 +0100

    if else

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
 fn main() {
     let mut x:i32;
     let mut y:i32;
+    let true_var: bool;/* Type */;
     x=43;
     y=9;
     x=21-78;
     y=multi(x,y);
-    println!("Hello, evgenii! : {} {}", add(x,y), multi(10,10));
+    println!("Hello, evgenii! : {} {}", bigger_or_equal(5,5), less(3,4));
 }
 fn add(a:i32,b:i32) -> i32 {
     a+b
 }
-fn multi(a:i32,b:i32) ->i32 {
+fn multi(a:i32,b:i32) -> i32 {
     a*b
+}
+fn less(a:i32,b:i32) -> bool {
+    if a < b { true } else { false }
+}
+fn bigger_or_equal(a:i32, b:i32) -> bool {
+    if a >= b { true } else { false }
 }
